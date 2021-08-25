029cf219983c9d3fe6cb3634f97414f45048e8b3
josh <j.maxwell.alan@gmail.com>
2021-08-25 16:11:38 -0400

    create kitties!

---
+++ assets/photos/index.js
@@ -0,0 +1,23 @@
+import kitty1 from './1.jpg';
+import kitty2 from './2.jpg';
+import kitty3 from './3.jpg';
+import kitty4 from './4.jpg';
+import kitty5 from './5.jpg';
+import kitty6 from './6.jpg';
+import kitty7 from './7.jpg';
+import kitty8 from './8.jpg';
+import kitty9 from './9.jpg';
+import kitty10 from './10.jpg';
+
+export default {
+  kitty1,
+  kitty2,
+  kitty3,
+  kitty4,
+  kitty5,
+  kitty6,
+  kitty7,
+  kitty8,
+  kitty9,
+  kitty10
+};
